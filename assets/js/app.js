dca2b57ed1f2c306e0a23fc525dd01a6600adf81
Bruno Coghlan <brunocoghlan@gmail.com>
2024-02-11 03:23:06 -0300

    8 funtion js done @media css done

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,6 @@ const table = document.querySelector('#table')
 const total = document.querySelector('#total')
 const realizada = document.querySelector('#realizada')
 const faltante = document.querySelector('#faltante')
-const check = document.querySelector('#check')
-const borrar = document.querySelector('#borrar')
-const editar = document.querySelector('#editar')
 const tareas = []
 /*Funciones*/
 /*crea un id unico y evita que se repita*/
@@ -15,19 +12,63 @@ const getRandom = () => {
     if (tareas.find((e) => e.id === id)) return getRandom()
     return id
 }
+/*revisa si una tarea fue completada o no, con true o false*/
+const completado = (id) => {
+    const index = tareas.findIndex((e) => e.id === id)
+    tareas[index].completado ? tareas[index].completado = false : tareas[index].completado = true
+    imprimir(tareas)
+}
+/*borra una tarea, preguntando si esta seguro, la borra del array, la vuelve a imprimir*/
+const borrar = (id) => {
+    const index = tareas.findIndex((e) => e.id === id)
+    if (confirm('Seguro que desea eliminar la tarea')) {
+        tareas.splice(index, 1)
+        imprimir(tareas)
+    }
+}
+/*checkea si la tarea esta completada o no, cada vez que se imprima, retorna un string vacio o checked*/
+const checked = (boolean) => {
+    if (boolean) return 'checked'
+}
+/*cuenta las tareas totales, realizadas y faltantes*/
+const contar = (array) => {
+    const cuenta = array.filter((array) => array.completado === true).length
+    total.innerHTML = array.length
+    realizada.innerHTML = cuenta
+    faltante.innerHTML = array.length - cuenta
+}
+/*edita la descripcion de una tarea, sin modificar el ID, preguntando con un confirm y remplazando con un string prompt*/
+const editar = (id, description) => {
+    const index = tareas.findIndex((e) => e.id === id)
+    if (confirm('Seguro que desea editar la tarea')) {
+        tareas[index].description = prompt('Edite su tarea', description)
+        imprimir(tareas)
+    }
+}
+/*define si el texto sera tachado por la tarea completada o no */
+const streaked = (boolean) => {
+    if (boolean) return 'line-through;"'
+}
+/*style="text-decoration:line-through;"
+guarda la informacion en el array, recorriendolo con un forEach, para luego imprimirlo en formato de tabla HTML*/
 const imprimir = (array) => {
-    let html =''
+    let html = ''
     array.forEach(e => {
-        html +=`
+        html += `
         <tr>
             <td>${e.id}</td>
-            <td>${e.description}</td>
-            <td><input id="check" type="checkbox"><button id="borrar"><i class="fa-solid fa-trash"></i></button><button id="editar"><i class="fa-solid fa-pen"></i></button></td>
+            <td style="text-decoration:${streaked(e.completado)};">${e.description}</td>
+            <td style="text-align:center">
+                <input type="checkbox" ${checked(e.completado)} onchange='completado(${e.id})' id="${e.id}">
+                <button onclick="borrar(${e.id})" class="btn"><i class="fa-solid fa-trash"></i></button>
+                <button class="btn" onclick="editar(${e.id},'${e.description}')"><i class="fa-solid fa-pen"></i></button>
+            </td>
         </tr>`
     });
     table.innerHTML = html
+    contar(array)
 }
-
+/*escucha al boton de agregar tareas, guarda las tareas en el array luego lo manda a imprimir.*/
 add.addEventListener('click', () => {
     if (input.value === '') return
     tareas.push({
@@ -38,3 +79,4 @@ add.addEventListener('click', () => {
     input.value = ''
     imprimir(tareas)
 })
+imprimir(tareas)
